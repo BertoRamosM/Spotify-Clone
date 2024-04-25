e3a43269bae3bb706c365b090e6dae8682cc91d0
ARamos <albertramos902@gmail.com>
2024-04-25 15:23:30 +0100

    play button with react created

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 export const Pause = ({ className }) => (
   <svg
     className={className}
@@ -24,11 +26,24 @@ export const Play = ({ className }) => (
   </svg>
 );
 const Player = () => {
+  const [isPlaying, setIsPlaying] = useState(false)
   return (
     <div className="flex flex-row justify-between w-full px-4 z-50">
       <div>Current Song</div>
-      <div className="">Reporoducer</div>
-      <div className="volume"></div>
+      <div className="grid place-content-center gap-4 flex-1">
+        <div className="flex justify-center">
+          <button className="bg-white rounded-full p-2"
+          onClick={()=>setIsPlaying(!isPlaying)}>
+        {isPlaying ? <Pause /> : <Play />}
+
+          </button>
+        </div>
+      </div>
+      <div className="grid place-content-center">
+
+      </div>
+
+      <audio ref
     </div>
   );
 };
