64bcc1af5cb629305a9c48a938734c4cea55b39b
ARamos <albertramos902@gmail.com>
2024-04-25 21:39:24 +0100

    single card gallery play button functional

--- src/components/CardPlayButton.jsx
+++ src/components/CardPlayButton.jsx
@@ -3,11 +3,16 @@ import { usePlayerStore } from "../store/playerStore"
 
 
 const CardPlayButton = ({ id }) => {
-  const {currentSong, isPlaying, setIsPlaying, setCurrentSong} = usePlayerStore(state => state)
+  const { currentSong, isPlaying, setIsPlaying, setCurrentSong } = usePlayerStore(state => state)
+  
+  const handleClick = () => {
+    setIsPlaying(!isPlaying)
+  }
   return (
-    <div className="card-play-button rounded-full bg-green-500 p-3">
+    <button className="card-play-button rounded-full bg-green-500 p-3"
+    onClick={handleClick}>
       {isPlaying ? <Pause/> : <Play/>}
-    </div>
+    </button>
   )
 }
 
