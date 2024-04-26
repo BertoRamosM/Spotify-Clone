1c092b1a8c642d863e41cd4b3179c4cb676db17f
ARamos <albertramos902@gmail.com>
2024-04-26 15:28:59 +0100

    handleclicks to mute and unmute still error, when after unmute the song starts at begining

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,6 +1,7 @@
 import { useState, useRef, useEffect } from "react";
 import { usePlayerStore } from "../store/playerStore";
 import { Slider } from "./Slider";
+
 export const Pause = ({ className }) => (
   <svg
     className={className}
@@ -35,38 +36,38 @@ const CurrentSong = ({ image, title, artists }) => {
       </picture>
       <div className="flex flex-col">
         <h3 className="font-bold block">{title}</h3>
-
         <p className="text-zinc-300 text-xs">{artists?.join(", ")}</p>
       </div>
     </div>
   );
-}
+};
 
-export const VolumeSilenceIcon = () => (
+export const VolumeSilenceIcon = ({ onClick }) => (
   <svg
     fill="currentColor"
     role="presentation"
     height="16"
     width="16"
     aria-hidden="true"
-    aria-label="Volumen apagado"
+    aria-label="Volume Muted"
     viewBox="0 0 16 16"
+    onClick={onClick}
   >
     <path d="M13.86 5.47a.75.75 0 0 0-1.061 0l-1.47 1.47-1.47-1.47A.75.75 0 0 0 8.8 6.53L10.269 8l-1.47 1.47a.75.75 0 1 0 1.06 1.06l1.47-1.47 1.47 1.47a.75.75 0 0 0 1.06-1.06L12.39 8l1.47-1.47a.75.75 0 0 0 0-1.06z"></path>
     <path d="M10.116 1.5A.75.75 0 0 0 8.991.85l-6.925 4a3.642 3.642 0 0 0-1.33 4.967 3.639 3.639 0 0 0 1.33 1.332l6.925 4a.75.75 0 0 0 1.125-.649v-1.906a4.73 4.73 0 0 1-1.5-.694v1.3L2.817 9.852a2.141 2.141 0 0 1-.781-2.92c.187-.324.456-.594.78-.782l5.8-3.35v1.3c.45-.313.956-.55 1.5-.694V1.5z"></path>
   </svg>
 );
 
-export const VolumeIcon = () => (
+export const VolumeIcon = ({ onClick }) => (
   <svg
     fill="currentColor"
     role="presentation"
     height="16"
     width="16"
     aria-hidden="true"
-    aria-label="Volumen alto"
-    id="volume-icon"
+    aria-label="High Volume"
     viewBox="0 0 16 16"
+    onClick={onClick}
   >
     <path d="M9.741.85a.75.75 0 0 1 .375.65v13a.75.75 0 0 1-1.125.65l-6.925-4a3.642 3.642 0 0 1-1.33-4.967 3.639 3.639 0 0 1 1.33-1.332l6.925-4a.75.75 0 0 1 .75 0zm-6.924 5.3a2.139 2.139 0 0 0 0 3.7l5.8 3.35V2.8l-5.8 3.35zm8.683 4.29V5.56a2.75 2.75 0 0 1 0 4.88z"></path>
     <path d="M11.5 13.614a5.752 5.752 0 0 0 0-11.228v1.55a4.252 4.252 0 0 1 0 8.127v1.55z"></path>
@@ -74,39 +75,55 @@ export const VolumeIcon = () => (
 );
 
 const Player = () => {
-  const {isPlaying, setIsPlaying, currentMusic} = usePlayerStore(state => state)
-  
+  const { isPlaying, setIsPlaying, currentMusic } = usePlayerStore(
+    (state) => state
+  );
+
   const audioRef = useRef();
+  const prevVolumeRef = useRef(50); // Initialize prevVolumeRef outside of the component
 
   const [volume, setVolume] = useState(0.5);
 
   useEffect(() => {
-    isPlaying ? 
-      audioRef.current.play() : 
-      audioRef.current.pause()
-  }, [isPlaying])
-  
-  useEffect(() => {
-    const { song, playlist, songs } = currentMusic
-    if (song) {
-      const src = `/music/${playlist?.id}/0${song.id}.mp3`
-      audioRef.current.src = src
-      audioRef.current.volume = volume
-      audioRef.current.play()
-    }
+    isPlaying ? audioRef.current.play() : audioRef.current.pause();
+  }, [isPlaying]);
 
-  },[currentMusic])
+  useEffect(() => {
+    const { song, playlist } = currentMusic;
+    if (song) {
+      const src = `/music/${playlist?.id}/0${song.id}.mp3`;
+      audioRef.current.src = src;
+      audioRef.current.volume = volume;
+      audioRef.current.play();
+    }
+  }, [currentMusic, volume]);
 
   const handleVolumeChange = (value) => {
-    const [newVolume] = value
+    const [newVolume] = value;
     const volumeValue = newVolume / 100;
-    setVolume(volumeValue)
-    audioRef.current.volume = volumeValue
+    setVolume(volumeValue);
+    prevVolumeRef.current = newVolume;
+    audioRef.current.volume = volumeValue;
+  };
+
+  const isVolumeSilence = volume < 0.1
+
+  const handleVolumeSilence = () => {
+    if (isVolumeSilence) {
+       setVolume(prevVolumeRef.current);
+    } else {
+      prevVolumeRef.current = volume
+      setVolume(0) 
+    }
   }
+
+
 
   const handleClick = () => {
     setIsPlaying(!isPlaying);
+     
   };
+
   return (
     <div className="flex flex-row justify-between w-full px-4 z-50">
       <div>
@@ -120,8 +137,14 @@ const Player = () => {
         </div>
       </div>
 
-      <div className="flex items-center gap-2">
-        {volume === 0 ? <VolumeSilenceIcon /> : <VolumeIcon />}
+      <div className="flex items-center gap-x-2">
+        <button onClick={handleVolumeSilence}>
+          {volume === 0 ? (
+            <VolumeSilenceIcon  />
+          ) : (
+            <VolumeIcon />
+          )}
+        </button>
         <div className="grid place-content-center">
           <Slider
             defaultValue={[50]}
