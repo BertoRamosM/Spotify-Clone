bc25310e36aae64ba1343113365765efed887144
ARamos <albertramos902@gmail.com>
2024-04-26 10:47:48 +0100

    appi created

--- src/pages/api/get-info-playlist.json.js
+++ src/pages/api/get-info-playlist.json.js
@@ -1,7 +1,24 @@
-import { allPlaylists, songs } from "../../lib/data";
+import { allPlaylists, songs as allSongs } from "../../lib/data";
 
 export async function GET({ params, request }) {
   //get the id from the url search params
-  const {url} = response
+  const { url } = response
+  const urlObject = new URL(url)
+  const id = urlObject.searchParams.get('id')
 
+  //we get the playlist and the songs
+  const playlist = allPlaylists.find((playlist) => playlist.id === id)
+  const songs = allSongs.filter(song => song.albumId === playlist?.albumId)
+
+  return new Response(JSON.stringify({ playlist, songs }), {
+    headers: { "content-type": "application/json" },
+  })
+
+
+
+
+  //another way of doing it if we dont have the completed url
+ /*  const queryString = url.split("?")[1]
+  const query = new URLSearchParams(queryString)
+  query.get('id') */
 }
